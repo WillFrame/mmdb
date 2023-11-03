44f079490843272107f1ba697c673a86d6fa6591
Matvey <mozhevm@gmail.com>
2023-11-03 22:00:57 +0600

    fix

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -2,10 +2,6 @@ import React, {FC} from 'react';
 import {StyledCard, Wrapper} from './styles';
 import AuthForm from '../../modules/auth-form';
 
-// return axios.get<ITransfersDto[]>('/mock/transfers.json').then((data) => {
-//     return data;
-// });
-
 const SignIn: FC = () => (
     <Wrapper>
         <StyledCard>
